775096bc083b29eeb9820e535908e72f774defa3
HansenYudistira <hansencancer27@gmail.com>
2023-03-26 09:43:46 +0700

    Challenge 4 tinggal ukuran gridnya

--- games.js
+++ games.js
@@ -5,7 +5,29 @@ const kertas = document.getElementById("kertas");
 const batuCom = document.getElementById("batu-com");
 const guntingCom = document.getElementById("gunting-com");
 const kertasCom = document.getElementById("kertas-com");
-var pilihan = document.getElementsByClassName("pilihan");
+const button1 = document.querySelector("#button1");
+const button2 = document.querySelector("#button2");
+const button3 = document.querySelector("#button3");
+const button4 = document.querySelector("#button4");
+
+button1.addEventListener("click", disabledButtons);
+button2.addEventListener("click", disabledButtons);
+button3.addEventListener("click", disabledButtons);
+button4.addEventListener("click", enabledButtons);
+
+function disabledButtons() {
+    button1.disabled = true;
+    button2.disabled = true;
+    button3.disabled = true;
+    button4.disabled = false;
+}
+
+function enabledButtons() {
+    button1.disabled = false;
+    button2.disabled = false;
+    button3.disabled = false;
+    button4.disabled = true;
+}
 
 function getComputerChoice() {
     var rps = ['r', 'p', 's'];
@@ -44,13 +66,16 @@ function ulang() {
     result.style.fontSize = '80px';
     result.style.transform = 'rotate(0deg)';
     result.innerText = 'VS';
+    gunting.style.background = 'none';
+    batu.style.background = 'none';
+    kertas.style.background = 'none';
+    guntingCom.style.background = 'none';
+    batuCom.style.background = 'none';
+    kertasCom.style.background = 'none';
 }
 
 function game(userChoice) {
     const computerChoice = getComputerChoice();
-    for( let i = 0; i < pilihan.length; i++) {
-        pilihan.disabled[i] = true;
-    }
     switch (userChoice + computerChoice) {
         case 'sp':
             gunting.style.background = '#c4c4c4';
