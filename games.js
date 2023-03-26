d8a6007654d6fc3085bed3b992c637829b38cf13
HansenYudistira <hansencancer27@gmail.com>
2023-03-26 21:41:51 +0700

    challenge 4 finish

--- games.js
+++ games.js
@@ -1,10 +1,9 @@
-var result = document.getElementById("result");
-const batu = document.getElementById("batu");
-const gunting = document.getElementById("gunting");
-const kertas = document.getElementById("kertas");
-const batuCom = document.getElementById("batu-com");
-const guntingCom = document.getElementById("gunting-com");
-const kertasCom = document.getElementById("kertas-com");
+const batu = document.getElementById("button1");
+const kertas = document.getElementById("button2");
+const gunting = document.getElementById("button3");
+const batuCom = document.getElementById("computerChoice1");
+const kertasCom = document.getElementById("computerChoice2");
+const guntingCom = document.getElementById("computerChoice3");
 const button1 = document.querySelector("#button1");
 const button2 = document.querySelector("#button2");
 const button3 = document.querySelector("#button3");
@@ -29,43 +28,104 @@ function enabledButtons() {
     button4.disabled = true;
 }
 
-function getComputerChoice() {
-    var rps = ['r', 'p', 's'];
-    const randomChoice = Math.floor(Math.random() * rps.length);
-    return rps[randomChoice];
+class game {
+    constructor(userChoice, computerChoice) {
+        this.userChoice = userChoice;
+        this.computerChoice = computerChoice;
+    }
+
+    result = document.getElementById("result");
+
+    rockpaperscissors(userChoice, computerChoice) {
+        switch (userChoice + computerChoice) {
+            case 'sp':
+                gunting.style.background = '#c4c4c4';
+                kertasCom.style.background = '#c4c4c4';
+                this.#userWin();
+                break;
+            case 'rs':
+                batu.style.background = '#c4c4c4';
+                guntingCom.style.background = '#c4c4c4';
+                this.#userWin();
+                break;
+            case 'pr':
+                kertas.style.background = '#c4c4c4';
+                batuCom.style.background = '#c4c4c4';
+                this.#userWin();
+                break;
+            case 'sr':
+                gunting.style.background = '#c4c4c4';
+                batuCom.style.background = '#c4c4c4';
+                this.#userLose();
+                break;
+            case 'rp':
+                batu.style.background = '#c4c4c4';
+                kertasCom.style.background = '#c4c4c4';
+                this.#userLose();
+                break;
+            case 'ps':
+                kertas.style.background = '#c4c4c4';
+                guntingCom.style.background = '#c4c4c4';
+                this.#userLose();
+                break;
+            case 'rr':
+                batu.style.background = '#c4c4c4';
+                batuCom.style.background = '#c4c4c4';
+                this.#Draw();
+                break;
+            case 'pp':
+                kertas.style.background = '#c4c4c4';
+                kertasCom.style.background = '#c4c4c4';
+                this.#Draw();
+                break;
+            case 'ss':
+                gunting.style.background = '#c4c4c4';
+                guntingCom.style.background = '#c4c4c4';
+                this.#Draw();
+                break;
+        }
+    }
+
+    getComputerChoice() {
+        var rps = ['r', 'p', 's'];
+        const randomChoice = Math.floor(Math.random() * rps.length);
+        return rps[randomChoice];
+    }
+
+    #userWin() {
+        this.result = this.#changeColor(this.result);
+        this.result.style.background = '#52CC7A';
+        this.result.innerText = 'PLAYER 1 WIN';
+    }
+
+    #userLose() {
+        this.result = this.#changeColor(this.result);
+        this.result.style.background = '#52CC7A';
+        this.result.innerText = 'COM WIN';
+    }
+
+    #Draw() {
+        this.result = this.#changeColor(this.result);
+        this.result.style.background = '#35824E';
+        this.result.innerText = 'DRAW';
+    }
+
+    #changeColor(result) {
+        result.style.color = "white";
+        result.style.fontSize = "36px";
+        result.style.transform = 'rotate(-30deg)';
+        return result;
+    }
 }
 
-function changeColor(result) {
-    result.style.color = "white";
-    result.style.fontSize = "36px";
-    result.style.transform = 'rotate(-30deg)';
-    return result;
-}
-
-function userWin() {
-    result = changeColor(result);
-    result.style.background = '#52CC7A';
-    result.innerText = 'PLAYER 1 WIN';
-}
-
-function userLose() {
-    result = changeColor(result);
-    result.style.background = '#52CC7A';
-    result.innerText = 'COM WIN';
-}
-
-function Draw() {
-    result = changeColor(result);
-    result.style.background = '#35824E';
-    result.innerText = 'DRAW';
-}
+let game1 = new game('n', 'n');
 
 function ulang() {
-    result.style.background = 'none';
-    result.style.color = 'red';
-    result.style.fontSize = '80px';
-    result.style.transform = 'rotate(0deg)';
-    result.innerText = 'VS';
+    game1.result.style.background = 'none';
+    game1.result.style.color = 'red';
+    game1.result.style.fontSize = '80px';
+    game1.result.style.transform = 'rotate(0deg)';
+    game1.result.innerText = 'VS';
     gunting.style.background = 'none';
     batu.style.background = 'none';
     kertas.style.background = 'none';
@@ -74,53 +134,7 @@ function ulang() {
     kertasCom.style.background = 'none';
 }
 
-function game(userChoice) {
-    const computerChoice = getComputerChoice();
-    switch (userChoice + computerChoice) {
-        case 'sp':
-            gunting.style.background = '#c4c4c4';
-            kertasCom.style.background = '#c4c4c4';
-            userWin();
-            break;
-        case 'rs':
-            batu.style.background = '#c4c4c4';
-            guntingCom.style.background = '#c4c4c4';
-            userWin();
-            break;
-        case 'pr':
-            kertas.style.background = '#c4c4c4';
-            batuCom.style.background = '#c4c4c4';
-            userWin();
-            break;
-        case 'sr':
-            gunting.style.background = '#c4c4c4';
-            batuCom.style.background = '#c4c4c4';
-            userLose();
-            break;
-        case 'rp':
-            batu.style.background = '#c4c4c4';
-            kertasCom.style.background = '#c4c4c4';
-            userLose();
-            break;
-        case 'ps':
-            kertas.style.background = '#c4c4c4';
-            guntingCom.style.background = '#c4c4c4';
-            userLose();
-            break;
-        case 'rr':
-            batu.style.background = '#c4c4c4';
-            batuCom.style.background = '#c4c4c4';
-            Draw();
-            break;
-        case 'pp':
-            kertas.style.background = '#c4c4c4';
-            kertasCom.style.background = '#c4c4c4';
-            Draw();
-            break;
-        case 'ss':
-            gunting.style.background = '#c4c4c4';
-            guntingCom.style.background = '#c4c4c4';
-            Draw();
-            break;
-    }
+function chosen(userChoice) {
+    game1.computerChoice = game1.getComputerChoice();
+    game1.rockpaperscissors(userChoice, game1.computerChoice);
 }
